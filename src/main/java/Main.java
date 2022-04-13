b3151b17caefb73172d1a5e7ad13b11e996a5d00
AnnaGitt <Anna2022GitWork@gmail.com>
2022-04-14 01:57:53 +0300

    Домашнее задание №17

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Svetofor.isPlayerWin(10)); //Задаём скорость
+        int[] playersSpeeds = new int[]{10, 5, 0, 1, 0}; //Задаём скорости игроков
+        System.out.println("Кол-во выбывших игроков: " + Svetofor.numberOfLosers(playersSpeeds)); // Вывод числа выбывших игроков
+        System.out.print("Скорости выбывших игроков: ");
+        Svetofor.printArray(Svetofor.speedsOfLoserPlayers(playersSpeeds)); //Вывод скоростей выбывших игроков
+        System.out.println("");
+        System.out.print("Скорости победивших игроков: ");
+        Svetofor.printArray(Svetofor.speedsOfWinnerPlayers(playersSpeeds)); //Вывод скоростей выбывших игроков
+
     }
+
 }
--- src/main/java/Svetofor.java
+++ src/main/java/Svetofor.java
@@ -1,16 +1,59 @@
 public class Svetofor {
     public static boolean isGreenLight = false;
 
-    public static String isPlayerWin(int speedOfPlayer) {
-        String result = "";
-        if (isGreenLight) {   // зеленый
-            result = "Игрок со скоростью " + speedOfPlayer + " выйграл, так как цвет сфетофора зелёный!";
-        } else if (speedOfPlayer > 0) { // красный
-            result = "Игрок со скоростью " + speedOfPlayer + " проиграл, так как цвет сфетофора красный!";
-        } else {
-            result = "Игрок со скоростью " + speedOfPlayer + " выйграл, так как цвет сфетофора красный!!";
-        }
-        return result;
+    //Функция для опеределения того, что игрок выбыл или нет
+    public static boolean isPlayerWin(int speedOfPlayer) {
+        boolean winOrNot;
+        if (isGreenLight) winOrNot = true;
+        else if (speedOfPlayer > 0) winOrNot = false;
+        else winOrNot = true;
+        return winOrNot;
     }
+
+    //Функция для подсчёта числа выбывших игроков
+    public static int numberOfLosers(int[] playersSpeeds) {
+        int numOfLosers = 0;
+        for (int i = 0; i < playersSpeeds.length; i++) {
+            if (!isPlayerWin(playersSpeeds[i])) { //Проверка на то, что игкрок проиграл через функцию "isPlayerWin"
+                numOfLosers++;
+            }
+        }
+        return numOfLosers;
+    }
+
+    //Функция для заполнения массива скоростями выбывщих игроков
+    public static int[] speedsOfLoserPlayers(int[] playersSpeeds) {
+        int[] speedsOfLoserPlayer = new int[numberOfLosers(playersSpeeds)]; //Создание массива для скоростей проигравших через функцию подсчёта проигравших
+        int arrayPosition = 0;
+        for (int i = 0; i < playersSpeeds.length; i++) {
+            if (!isPlayerWin(playersSpeeds[i])) { //Проверка на то, что игкрок проиграл через функцию "isPlayerWin"
+                speedsOfLoserPlayer[arrayPosition] = playersSpeeds[i];
+                arrayPosition++;
+            }
+        }
+        return speedsOfLoserPlayer;
+    }
+
+    //Функция для заполнения массива скоростями победивших игроков
+    public static int[] speedsOfWinnerPlayers(int[] playersSpeeds) {
+        int[] speedsOfWinnerPlayers = new int[playersSpeeds.length - numberOfLosers(playersSpeeds)]; //Создание массива для скоростей проигравших через функцию подсчёта проигравших минус общее кол-во игроков
+        int arrayPosition = 0;
+        for (int i = 0; i < playersSpeeds.length; i++) {
+            if (isPlayerWin(playersSpeeds[i])) {
+                speedsOfWinnerPlayers[arrayPosition] = playersSpeeds[i];
+                arrayPosition++;
+            }
+        }
+        return speedsOfWinnerPlayers;
+    }
+
+    //Функция для вывода элементов массива
+    public static void printArray(int[] arrayForPrint) {
+        for (int i = 0; i < arrayForPrint.length; i++) {
+            System.out.print(arrayForPrint[i] + " ");
+        }
+    }
+
+
 }
 
